6cc3e72c329ad55dc602fc4e3db2d325597ae14a
JoeKami <j.b.carrasquillo@gmail.com>
2025-04-21 19:39:09 -0400

    Initializing project

---
+++ src/test/java/com/coderscampus/PollyChat/assignment142/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.PollyChat.assignment142;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
